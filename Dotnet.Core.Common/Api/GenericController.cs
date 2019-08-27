ae6ef53b275ed6312eb43d77398bd04e06b3e094
karabuluthakan <hakan.karabulut@ugurbilgi.net>
2019-08-27 11:25:13 +0300

    generic controller

--- Dotnet.Core.Common/Api/GenericController.cs
+++ Dotnet.Core.Common/Api/GenericController.cs
@@ -171,46 +171,6 @@ namespace Dotnet.Core.Common.Api
             }
         }
 
-        [HttpPatch]
-        public virtual async Task<IActionResult> UpdatePatchAsync([FromRoute] int id, TEditDto item)
-        {
-            var result = new Result<TEntity>();
-            var data = await _service.GetFindByIdAsync(id);
-            if (data == null)
-            {
-                result.ResultMessage = $"No records with id => {id}";
-                result.ResultInnerMessage = $"There is no record with this id => {id} in the '{ClassFullName}' model";
-                result.ResultCode = (int) ResultStatusCode.NotFound;
-                result.ResultStatus = false;
-                result.ResultObject = null;
-                return NotFound(result);
-            }
-
-            var mapping = _mapper.Map(item, data);
-
-            try
-            {
-                var update = _service.Update(mapping);
-                result.ResultObject = mapping;
-                result.ResultCode = update.ResultCode;
-                result.ResultStatus = update.ResultStatus;
-                result.ResultMessage = update.ResultMessage;
-                result.ResultInnerMessage = update.ResultInnerMessage;
-                if (update.ResultCode == (int) ResultStatusCode.Updated)
-                    return Ok(result);
-                return BadRequest(result);
-            }
-            catch (Exception e)
-            {
-                result.ResultObject = mapping;
-                result.ResultCode = (int) ResultStatusCode.BadRequest;
-                result.ResultStatus = false;
-                result.ResultMessage = e.Message;
-                result.ResultInnerMessage = e.InnerException?.ToString();
-                return BadRequest(result);
-            }
-        }
-
         [HttpDelete]
         public virtual async Task<IActionResult> DeleteAsync([FromRoute] int id)
         {
